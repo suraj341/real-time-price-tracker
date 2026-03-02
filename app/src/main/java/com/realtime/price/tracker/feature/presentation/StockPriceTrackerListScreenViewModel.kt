0c237794ba7e89ef7c3b5d1556648567305b7080
Suraj Prakash <sahana@Surajs-MacBook-Pro.local>
2026-03-02 16:48:04 +0530

    Created initial version of ViewModel

--- app/src/main/java/com/realtime/price/tracker/feature/presentation/StockPriceTrackerListScreenViewModel.kt
+++ app/src/main/java/com/realtime/price/tracker/feature/presentation/StockPriceTrackerListScreenViewModel.kt
@@ -1,4 +1,27 @@
 package com.realtime.price.tracker.feature.presentation
 
-class StockPriceTrackerListScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.realtime.price.tracker.feature.data.StockDetailsResult
+import com.realtime.price.tracker.feature.data.dto.StockDetailModel
+import com.realtime.price.tracker.feature.domain.StockPriceDetailsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class StockPriceTrackerListScreenViewModel(private val stockPriceDetailsUseCase: StockPriceDetailsUseCase) :
+    ViewModel() {
+    private val _state = MutableStateFlow<List<StockDetailModel>>(emptyList())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            stockPriceDetailsUseCase.observeStockPriceDetails()
+                .collect { result ->
+                    if (result is StockDetailsResult.Success) {
+                        _state.value = result.stocks
+                    }
+                }
+        }
+    }
 }
