2c656f271cdc0e430f2769f79f5eccf41244f0eb
Suraj Prakash <sahana@Surajs-MacBook-Pro.local>
2026-03-02 07:18:09 +0530

    Created initial dto to capture the stock details from the socket

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/realtime/price/tracker/feature/data/dto/StockDetailResponseModel.kt
+++ app/src/main/java/com/realtime/price/tracker/feature/data/dto/StockDetailResponseModel.kt
@@ -1,4 +1,10 @@
 package com.realtime.price.tracker.feature.data.dto
 
-class StockDetailResponseModel {
-}
+data class StockDetailResponseModel(
+    val symbol: String,
+    val name: String,
+    val price: Double,
+    val currency: String,
+    val details: String
+)
+
